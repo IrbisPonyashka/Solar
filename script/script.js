2edb3737c8ffc804a8b7bf67311fa9baaf7fd21c
Ismail <Ismail>
2022-07-12 16:30:23 +0500

    localStorage

--- script/script.js
+++ script/script.js
@@ -1,26 +1,42 @@
 const toggle = document.querySelector('.toggle'),
       svitch = document.querySelector('.switch'),
-      btn = document.querySelector('.btn');
-let root = document.documentElement;
+      btn = document.querySelector('.btn'),
+      body = document.querySelector('.body');
+let root = document.documentElement;  
+
 
 toggle.addEventListener('click', () => {
+  let theme = '';
+  let pos = '';
 
-  if(btn.classList.value !== 'btn active'){
-    root.style.setProperty('--black', '#ffffff');
-    root.style.setProperty('--greyDark', '#ffffff');
-    root.style.setProperty('--grey', '#ffffff');
-    root.style.setProperty('--white', '#292929');
-    btn.classList.add('active');
+  if(body.classList.contains('light')){
+    body.classList.remove('light');
+    body.classList.add('dark');
+    btn.classList.remove('right');
+    btn.classList.add('left');
+    pos = 'left';
+    theme ='dark';
   }
-    else if(btn.classList.value == 'btn active'){
-      root.style.setProperty('--black', '#000000');
-      root.style.setProperty('--greyDark', '#303030');
-      root.style.setProperty('--grey', '#868585');
-      root.style.setProperty('--white', '#fff');
-
-      btn.classList.remove('active');
+    else if(body.classList.contains('dark')){
+      body.classList.remove('dark');
+      body.classList.add('light');
+      btn.classList.remove('left');
+      btn.classList.add('right');
+      pos = 'right';
+      theme ='light';
     }
-    
+    localStorage.setItem('--position',pos)
+    localStorage.setItem('--theme',theme)
 })
-
-localStorage.setItem(toggle,'click');
+document.addEventListener('DOMContentLoaded', () => {
+  const val = localStorage.getItem('--theme'),
+        batn = localStorage.getItem('--position');
+  if(val){
+    body.classList.add(val);
+    btn.classList.add(batn);
+  }else{
+    body.classList.add('light'); 
+    btn.classList.add('right');
+  }
+  
+})
